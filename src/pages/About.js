839dfe115945434fcc205e64342057949dbe1244
Donny <66758569+donnycavalcante@users.noreply.github.com>
2022-05-13 21:45:16 -0300

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+        consequuntur praesentium, harum sint corporis odit ipsa, hic unde nisi,
+        eum qui aspernatur molestias. Debitis modi iste aliquid rem dolor!
+        Pariatur non praesentium, et debitis corrupti blanditiis earum cumque
+        eaque minima.
+      </p>
+    </section>
+  );
+};
+
+export default About;
